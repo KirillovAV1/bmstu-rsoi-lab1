c48a86361db21e3bba74ff617005f459117a3c24
aleksandr <a.vl.kirillov@gmail.com>
2025-09-29 11:52:19 +0300

    bug fix

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,8 +1,8 @@
 import os
 import pytest
 from sqlalchemy import text
-from run import create_app
-from model import db
+from server.run import create_app
+from server.model import db
 
 
 @pytest.fixture(scope="session")
